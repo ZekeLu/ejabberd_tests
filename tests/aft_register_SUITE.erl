c172ffb70cacd7193a20b251c90233010e5e42e5
Sharp Wang <sharp.wang@afusion.com>
2014-12-29 08:59:36 +0600

    modify activation

--- tests/aft_register_SUITE.erl
+++ tests/aft_register_SUITE.erl
@@ -51,7 +51,7 @@ end_per_group(_GroupName, _Config) ->
     ok.
 
 init_per_testcase(change_password, Config) ->
-    do_register(Config, phone),
+    do_register(Config, phone, <<"phone">>),
     [{new_password, <<"deih38js">>} | Config];
 init_per_testcase(_CaseName, Config) ->
     Config.
@@ -60,8 +60,6 @@ end_per_testcase(register_with_phone, Config) ->
     delete_user(Config, phone);
 end_per_testcase(register_with_email, Config) ->
     delete_user(Config, email);
-end_per_testcase(register_with_phone_and_email, Config) ->
-    delete_user(Config, phone_and_email);
 end_per_testcase(change_password, Config) ->
     {_Name, UserSpec} = get_aft_user_by_name(phone),
     NewUserSpec = proplists:delete(password, UserSpec),
@@ -77,30 +75,34 @@ end_per_testcase(_CaseName, _Config) ->
 register_with_phone(Config) ->
     {value, Registarations} = get_counter_value(modRegisterCount),
 
-    do_register(Config, phone),
+    do_register(Config, phone, <<"phone">>),
 
     assert_counter(Registarations + 1, modRegisterCount).
 
 register_with_email(Config) ->
     {value, Registarations} = get_counter_value(modRegisterCount),
 
-    do_register(Config, email),
+    do_register(Config, email, <<"email">>),
 
     assert_counter(Registarations + 1, modRegisterCount).
 
 register_with_phone_and_email(Config) ->
     {value, Registarations} = get_counter_value(modRegisterCount),
 
-    do_register(Config, phone_and_email),
+    do_register(Config, phone_and_email, <<"phone">>),
 
-    assert_counter(Registarations + 1, modRegisterCount).
+    assert_counter(Registarations, modRegisterCount),
+
+    {value, Registarations1} = get_counter_value(modRegisterCount),
+
+    do_register(Config, phone_and_email, <<"email">>),
+
+    assert_counter(Registarations1, modRegisterCount).
 
 register_with_weak_password(Config) ->
     {value, Registarations} = get_counter_value(modRegisterCount),
 
-    Result = do_register(Config, weak_password),
-
-    {ok, modify, _} = Result,
+    do_register(Config, weak_password, <<"phone">>),
 
     assert_counter(Registarations, modRegisterCount).
 
@@ -123,18 +125,29 @@ change_password(Config) ->
 %%--------------------------------------------------------------------
 %% Helpers
 %%--------------------------------------------------------------------
-do_register(Config, Name) ->
+
+do_register(Config, Name, Type) ->
     {_Name, UserSpec} = get_aft_user_by_name(Name),
 
     ClientProps = escalus_users:get_options(Config, UserSpec),
     {ok, Conn, ClientProps, _} = escalus_connection:start(ClientProps,
-                                                          [start_stream,
-                                                           stream_features,
-                                                           maybe_use_ssl]),
+        [start_stream,
+            stream_features,
+            maybe_use_ssl]),
     escalus_connection:send(Conn, register_account(UserSpec)),
-    Result = wait_for_result(Conn),
-    escalus_connection:stop(Conn),
-    Result.
+    case wait_for_result(Conn) of
+        {ok, result, _Stanza} ->
+            case Type of
+                <<"phone">> ->
+                    escalus_connection:send(Conn, active_phone(UserSpec) ),
+                    wait_for_result(Conn);
+                <<"email">> ->
+                    active_email(UserSpec)
+            end;
+        _->
+            error
+    end,
+    escalus_connection:stop(Conn).
 
 wait_for_result(Conn) ->
     receive
@@ -175,6 +188,26 @@ response_type(#xmlel{name = <<"iq">>} = IQ) ->
 response_type(_) ->
     other.
 
+active_email(UserSpec) ->
+    Email = proplists:get_value(email, UserSpec),
+    Token = get_token( Email ),
+    send_http_request( Token, Email ).
+
+
+active_phone(UserSpec) ->
+    PhoneEl = [#xmlel{name = atom_to_binary(Key, latin1), children = [exml:escape_cdata(proplists:get_value(Key, UserSpec))]}
+        || Key <- [phone], proplists:is_defined(Key, UserSpec)],
+
+    Phone = proplists:get_value(phone, UserSpec),
+    Token = get_token( Phone ),
+    CodeEl = #xmlel{name = <<"code">>, children = [#xmlcdata{content = Token}]},
+    Elements = [PhoneEl, CodeEl],
+
+    escalus_stanza:iq(<<"set">>,
+        [#xmlel{name = <<"query">>, attrs = [{<<"xmlns">>, <<"aft:register">>}],
+            children = Elements}]).
+
+
 register_account(UserSpec) ->
     Elements = [#xmlel{name = atom_to_binary(Key, latin1), children = [exml:escape_cdata(proplists:get_value(Key, UserSpec))]}
                 || Key <- [phone, email, nick, password], proplists:is_defined(Key, UserSpec)],
@@ -208,3 +241,38 @@ delete_user(Config, UserSpec) ->
     escalus_connection:send(Conn, remove_account()),
     wait_for_result(Conn),
     escalus_connection:stop(Conn).
+
+send_http_request(Token, Email) ->
+    inets:start(),
+    Method = get,
+    TokenString = binary_to_list( base64:encode(<<Token/binary, $@, Email/binary>>) ),
+    URL = "http://localhost:5280/verify?token=" ++ TokenString,
+    Header = [],
+    HTTPOptions = [],
+    Options = [{sync, true}],
+    case httpc:request(Method, {URL, Header}, HTTPOptions, Options) of
+        {ok,{{"HTTP/1.1",200,"OK"}, _} } ->
+            ok;
+        _ ->
+            error
+    end.
+
+% query token from redis.
+% redis server port: 6379
+get_token(Key) ->
+    {ok, Socket} = gen_tcp:connect( "localhost", 6379, [binary, {active, false}, {packet, 0}]),
+    Size = integer_to_binary( iolist_size( Key ) ),
+    CMD = <<"*2\r\n$4\r\nMGET\r\n$", Size/binary, "\r\n", Key/binary, "\r\n\r\n">>,
+    ok = gen_tcp:send( Socket, CMD ),
+    {ok, Packet} = gen_tcp:recv( Socket, 0),
+    ok = gen_tcp:close( Socket ),
+    [_,_,Value,_] = binary:split( Packet, <<"\r\n">>, [global] ),
+    case binary:split(Value, <<":">>, [global] ) of
+        [_Type, _GUID, _Password, _Nick, Token] ->
+            Token;
+        _ ->
+            error
+    end.
+
+
+
